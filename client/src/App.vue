45d7e7705f6d48e9fd39e643a7f0a7c00a106ba2
jnguyen <zfv431@my.utsa.edu>
2020-03-30 14:24:04 -0500

    developing home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
-}
+};
 </script>
 
 <style>
