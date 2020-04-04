303e229c17c7613680b7afd3b48ee17cb3bdc214
jnguyen <zfv431@my.utsa.edu>
2020-04-04 18:41:20 -0500

    Navigation Working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <Home />
+  <div>
+    <NavBar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import Home from './pages/Home';
+//import Home from './pages/Home';
+import NavBar from './components/NavigationBar';
 export default {
   name: 'app',
   components: {
-    Home: Home,
+    NavBar: NavBar,
   },
 };
 </script>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
-    <NavBar />
     <p>This is the home page</p>
   </div>
 </template>
 
 <script>
-import NavBar from '../components/NavigationBar';
+//import NavBar from '../components/NavigationBar';
 export default {
   name: 'Home',
-  components: {
-    NavBar: NavBar,
-  },
 };
 </script>
 
