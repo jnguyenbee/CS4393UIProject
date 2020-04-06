ba6dcea460c94c3ecfd5ee02450128da12ab2d27
jnguyen <zfv431@my.utsa.edu>
2020-04-05 22:17:09 -0500

    mistakes were madee

--- client/src/components/product/ProductList.vue
+++ client/src/components/product/ProductList.vue
@@ -13,20 +13,6 @@
         </div>
       </div>
     </div>
-    <b-sidebar id="product.id" title="Sidebar" right shadow>
-      <div class="px-3 py-2">
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-        <b-img
-          src="https://picsum.photos/500/500/?image=54"
-          fluid
-          thumbnail
-        ></b-img>
-      </div>
-    </b-sidebar>
   </div>
 </template>
 
