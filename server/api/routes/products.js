fd1f6784db3cf8b9bf5edc9323ced2a431947f61
kolleensalinda <43282975+kolleensalinda@users.noreply.github.com>
2020-04-09 18:28:13 -0500

    added routes and changed product schema

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -4,19 +4,14 @@ const mongoose = require('mongoose');
 
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
-    Product.find()
-    .exec()
-    .then(docs => {
-        res.status(200).json(docs);
-    })
-});
-
 router.post('/', (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price,
+        size: req.body.size,
+        color: req.body.color,
+        quantity: req.body.quantity,
         description: req.body.description,
     });
     product.save().then(result => {
@@ -32,6 +27,14 @@ router.post('/', (req, res, next) => {
             error: err
         });
     });
+});
+
+router.get('/', (req, res, next) => {
+    Product.find()
+    .exec()
+    .then(docs => {
+        res.status(200).json(docs);
+    })
 });
 
 router.get('/:productId', (req, res, next) => {
@@ -59,12 +62,16 @@ router.patch('/:productId', (req, res, next) => {
     for (const ops of req.body){
         updateOps[ops.propName] = ops.value;
     }
-
     Product.update({ _id : id}, { $set: updateOps })
         .exec()
         .then(result => {
             res.status(200).json(result);
         })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
