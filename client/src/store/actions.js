bffbc2ebb0e7e4350885e5da654bca5d4ba40615
jnguyen <zfv431@my.utsa.edu>
2020-04-09 15:36:07 -0500

    font awesome working...

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -6,6 +6,10 @@ import {
     PRODUCT_BY_ID_SUCCESS,
     ALL_PRODUCTS,
     ALL_PRODUCTS_SUCCESS,
+    UPDATE_PRODUCT,
+    UPDATE_PRODUCT_SUCCESS,
+    REMOVE_PRODUCT,
+    REMOVE_PRODUCT_SUCCESS,
 } from './mutation-types';
 
 const API_BASE = 'http://localhost:3000';
@@ -29,4 +33,17 @@ export const productActions = {
             commit(ADD_PRODUCT_SUCCESS, response.data);
         });
     },
+    updateProduct({ commit }, payload) {
+        commit(UPDATE_PRODUCT)
+        axios.put(`${API_BASE}/products/${payload._id}`, payload).then(response => {
+            commit(UPDATE_PRODUCT_SUCCESS, response.data)
+        })
+    },
+    removeProduct({ commit }, payload) {
+        commit(REMOVE_PRODUCT)
+        axios.delete(`${API_BASE}/products/${payload}`, payload).then(response => {
+            console.debug('response', response.data)
+            commit(REMOVE_PRODUCT_SUCCESS, response.data)
+        })
+    }
 };
