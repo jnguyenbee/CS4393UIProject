986e86179d7dd70885cc344a9bd39ad2e2c7aa73
jnguyen <zfv431@my.utsa.edu>
2020-04-09 18:03:53 -0500

    working fix

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -9,9 +9,9 @@ const storage = multer.diskStorage({
     },
     filename: function(req, file, cb) {
         cb(null, file.originalname);
-    }
+    },
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
@@ -19,23 +19,23 @@ router.get('/', (req, res, next) => {
         .exec()
         .then((docs) => {
             res.status(200).json(docs);
-        }
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err,
+            });
         });
-    });
 });
 
-router.post('/', upload.single('productImage'),(req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
     console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price,
         description: req.body.description,
-        productImage: req.file.path
+        productImage: req.file.path,
     });
     product
         .save()
@@ -57,19 +57,20 @@ router.post('/', upload.single('productImage'),(req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-    .exec()
-    .then(doc => {
-    console.log("from database", doc);
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        res.status(404)
-          .json({ message: "No valid entry found for provided ID" });
-      }
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
+        .exec()
+        .then((doc) => {
+            console.log('from database', doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: 'No valid entry found for provided ID' });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err,
+            });
         });
 });
 
@@ -84,12 +85,12 @@ router.put('/:productId', (req, res, next) => {
         });
 
     /*
-            for (const ops of req.body) {
-                updateOps[ops.propName] = ops.value;
-            }
+                          for (const ops of req.body) {
+                              updateOps[ops.propName] = ops.value;
+                          }
 
-          Product.update({ _id: id }, { $set: updateOps })
-              */
+                        Product.update({ _id: id }, { $set: updateOps })
+                            */
 });
 
 router.delete('/:productId', (req, res, next) => {
