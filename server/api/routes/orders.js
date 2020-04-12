f68b107aa0c74ea1c448e6fb487fcec33c2ed4b1
Joe Garcia <gijoe250@gmail.com>
2020-04-12 06:48:24 -0500

    Update orders.js
    order is not completed. suggest not using yet

--- server/api/routes/orders.js
+++ server/api/routes/orders.js
@@ -3,26 +3,27 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
+const Product = require('../models/product');
 
-/*router.get('/', (req, res, next) => {
-    Product.find()
+
+router.get('/', (req, res, next) => {
+    Order.find()
+    .select('product quantity _id')
     .exec()
     .then(docs => {
-        res.status(200).json(docs);
-    })
-});
-*/
-router.post('/', (req, res, next) => {
-    const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        quantity: req.body.quantity,
-        product: req.body.productId
-    });
-    order.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'posted product to products db',
-            orderInfo: order
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map( doc => {
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+                }
+            })
         });
     })
     .catch(err => {
@@ -32,10 +33,48 @@ router.post('/', (req, res, next) => {
         });
     });
 });
-/*
+
+router.post('/', (req, res, next) => {
+    Product.findById(req.body.productId)
+        .then(product => {
+            if (!product) {
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            })
+            return order.save();
+        })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+            message: 'posted order to orders db',
+            orderInfo: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity,
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders/' + result._id
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    Product.findById(id)
+    Order.findById(id)
     .exec()
     .then(doc => {
       if (doc) {
@@ -51,7 +90,7 @@ router.get('/:productId', (req, res, next) => {
         });
     });
 });
-*/
+
 /*router.patch('/:boardgameId', (req, res, next) => {
     const id = req.params.boardgameId;
     const updateOps = {};
