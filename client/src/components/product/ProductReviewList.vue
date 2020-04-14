ba4acec7817b5d64a105a57de541c4cb03fe82a1
jnguyen <zfv431@my.utsa.edu>
2020-04-14 02:04:08 -0500

    review path 40^

--- client/src/components/product/ProductReviewList.vue
+++ client/src/components/product/ProductReviewList.vue
@@ -1,11 +1,23 @@
 <template>
-  <p>NEED TO GET ID AND SEND TO TEMPLE VIA PRODUVT VIEW; SEE PRODUCT LIST, PRODUCT ITEMS FOR ASSISTANCE</p>
+  <div>
+    <p>NEED TO GET ID AND SEND TO TEMPLE VIA PRODUVT VIEW; SEE PRODUCT LIST, PRODUCT ITEMS FOR ASSISTANCE</p>
+    <p>NEED TO PASS IN REVIEW NOT PRODUCT</p>
+    <p>
+      <product-review product="product"></product-review>
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
 
 <script>
+import ProductReview from "./ProductReview";
 export default {
-  props: ["product"]
+  props: ["product"],
+  components: {
+    "product-review": ProductReview
+    //"product-back": ProductBackButton
+    //  "product-review-side-button": ProductReviewButton
+  }
 };
 </script>
