488a6c6b4f44c0147a512ada3e6f1ea5a1c95a5a
jnguyen <zfv431@my.utsa.edu>
2020-04-13 21:42:35 -0500

    update cart ui

---
+++ client/src/components/user/AddressForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-form>
+    <h6>shipping address</h6>
+    <b-form-group id="input-group-1" label="Username :" label-for="input-1">
+      <b-form-input id="input-1" type="user" required placeholder="Enter username"></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+      <b-form-input id="input-2" type="password" required placeholder="Enter password"></b-form-input>
+    </b-form-group>
+
+    <h6>billing address</h6>
+    <b-form-group id="input-group-1" label="Username :" label-for="input-1">
+      <b-form-input id="input-1" type="user" required placeholder="Enter username"></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+      <b-form-input id="input-2" type="password" required placeholder="Enter password"></b-form-input>
+    </b-form-group>
+
+    <h5>payment</h5>
+    <b-form-group id="input-group-1" label="Username :" label-for="input-1">
+      <b-form-input id="input-1" type="user" required placeholder="Enter username"></b-form-input>
+    </b-form-group>
+    <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+      <b-form-input id="input-2" type="password" required placeholder="Enter password"></b-form-input>
+    </b-form-group>
+  </b-form>
+</template>
+
+<script></script>
+<style scoped>
+@import "../../../../client/static/app.css";
+</style>
--- client/src/pages/Cart.vue
+++ client/src/pages/Cart.vue
@@ -4,6 +4,16 @@
       <h1>
         <i class="fa fa-shopping-cart"></i> Your Cart
       </h1>
+      <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+      <b-modal id="modal-1" hide-footer title="Purchasing Order">
+        <addressForm />
+        <template v-for="product in cart">
+          <p :key="product.id">{{product.name}} {{product.price}}</p>
+        </template>
+        <b-button class="mt-3" variant="outline-danger" block>Cancel</b-button>
+        <b-button class="mt-2" variant="outline-warning" block>Continue</b-button>
+      </b-modal>
     </div>
     <template v-for="product in cart">
       <product-details :product="product" :key="product.id"></product-details>
@@ -18,6 +28,7 @@
 
 <script>
 import ProductDetails from "../components/product/ProductDetailsCart";
+import AddressForm from "../components/user/AddressForm";
 export default {
   data() {
     return {
@@ -25,7 +36,8 @@ export default {
     };
   },
   components: {
-    productDetails: ProductDetails
+    productDetails: ProductDetails,
+    addressForm: AddressForm
   }
 };
 </script>
