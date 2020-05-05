8061735e9bfb4f7faaa31bd1300dba02d550db53
jnguyen <zfv431@my.utsa.edu>
2020-05-05 05:31:24 -0500

    3000 was heruoku traffic

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 console.log('port' + port);
 const server = http.createServer(app);
