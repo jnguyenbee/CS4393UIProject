de26f14561a4ddbbe38e94da7d6c10d09bb100cb
jnguyen <zfv431@my.utsa.edu>
2020-05-05 05:39:02 -0500

    listen to port condolse

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
 
-const port = process.env.PORT || 5000;
+var server = app.listen(process.env.PORT || 5000, function() {
+    var port = server.address().port;
+    console.log('Express is working on port ' + port);
+});
 
-console.log('port' + port);
 const server = http.createServer(app);
-
-server.listen(port);
