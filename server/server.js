c8e211b516dd2c4ac6338be38f9afd3a2d9bb02e
jnguyen <zfv431@my.utsa.edu>
2020-05-05 00:16:24 -0500

    heruko
    test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
-
+const serveStatic = require('serve-static');
+const path = require('path');
+app.use('/', serveStatic(path.join('/client/ dist ')));
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
