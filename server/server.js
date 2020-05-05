163914c0bddff2b2fc3a3ed259f4f98b93dee396
jnguyen <zfv431@my.utsa.edu>
2020-05-05 14:25:07 -0500

    i'm hunfry

--- server/server.js
+++ server/server.js
@@ -3,9 +3,8 @@ const app = require('./app');
 const dotenv = require('dotenv').config();
 
 const server = http.createServer(app);
-var host = server.address().address;
-host = host == '::' ? 'localhost' : host;
-server.listen(process.env.PORT || 5000, host);
+
+server.listen(process.env.PORT || 5000, 'localhost');
 console.log('Express server started on port %s', server.address().port);
 console.log('Express server started on addree %s', server.address().address);
 //var port = process.env.PORT;
