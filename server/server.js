b050134c6fcfc538ff18830c7ef4cf4033401b31
jnguyen <zfv431@my.utsa.edu>
2020-05-05 14:56:13 -0500

    i'm hunfry

--- server/server.js
+++ server/server.js
@@ -3,21 +3,7 @@ const app = require('./app');
 const dotenv = require('dotenv').config();
 var PORT = process.env.PORT || 5000;
 
-var http = require('http');
-var server = http.Server(app);
-//const server = http.createServer(app);
-//var io = require('socket.io').listen(server);
+const server = http.createServer(app);
+server.listen(PORT);
 
-server.listen(PORT, function() {
-    console.log('Sock server running');
-});
-
-//console.log('Express server started on port %s', server.address().port);
-//console.log('Express server started on addree %s', server.address().address);
-var io = require('socket.io')(server);
-
-io.on('connection', function(socket) {
-    socket.on('message', function(msg) {
-        io.emit('message', msg);
-    });
-});
+module.exports = app;
