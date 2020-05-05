e75525fffab1f4448a4d32b6e26655dc9eba77a3
jnguyen <zfv431@my.utsa.edu>
2020-05-05 05:19:43 -0500

    port flex

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const dotenv = require('dotenv').config();
 
 module.exports = {
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:3000',
+                target: 'http://localhost:' + process.env.PORT,
                 changeOrigin: true,
                 logLevel: 'debug',
                 pathRewrite: { '^/api': '/' },
