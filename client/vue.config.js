54dba35d5d122d5e6cd04efe89c1a64eebc195b5
jnguyen <zfv431@my.utsa.edu>
2020-05-05 15:03:54 -0500

    i'm hunfry

--- client/vue.config.js
+++ client/vue.config.js
@@ -5,11 +5,14 @@ module.exports = {
     outputDir: path.resolve(__dirname, '../server/public'),
     devServer: {
         proxy: {
-            '^product/': {
+            '^/product': {
                 //target: 'http://localhost:' + JSON.stringify(5000),
                 target: 'http://localhost:' + JSON.stringify(process.env.PORT),
                 changeOrigin: true,
                 logLevel: 'debug',
+                pathRewrite: {
+                    '^/product': '',
+                },
             },
             '^/users/': {
                 //target: 'http://localhost:' + JSON.stringify(5000),
