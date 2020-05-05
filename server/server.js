b058f0284f7457881ea4a00a99b4d859f8583061
jnguyen <zfv431@my.utsa.edu>
2020-05-05 00:22:19 -0500

    fix the dir path

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const app = require('./app');
 const dotenv = require('dotenv').config();
 const serveStatic = require('serve-static');
 const path = require('path');
-app.use('/', serveStatic(path.join('/client/ dist ')));
+
+app.use('/', serveStatic(path.join(process.cwd(), 'client/dist ')));
+var paths = path.join(process.cwd(), 'client/dist ');
+console.log(paths);
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
