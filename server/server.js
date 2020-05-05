3fced82ccebf69e597efa0bc377bcd5f72cf9cf8
jnguyen <zfv431@my.utsa.edu>
2020-05-05 05:41:52 -0500

    console lgo

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
 
-var server = app.listen(process.env.PORT || 5000, function() {
-    var port = server.address().port;
+var ser = app.listen(process.env.PORT || 5000, function() {
+    var port = ser.address().port;
     console.log('Express is working on port ' + port);
 });
 
