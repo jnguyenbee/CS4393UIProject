08f0a1dedde94639ab887f4cfceee01fb81f867d
jnguyen <zfv431@my.utsa.edu>
2020-05-05 16:01:12 -0500

    i'm hunfry

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const dotenv = require('dotenv').config();
 var PORT = process.env.PORT || 5000;
-
+console.log(PORT);
 const server = http.createServer(app);
 server.listen(PORT);
 
